ff236164c27106bdf9938c96fc95e6d38e78bec4
I539231 <pascal.zimmermann01@sap.com>
2023-08-16 12:31:41 +0200

    feat: Start the Gorm migration

--- migration/V4_add_rg_uniq_idx_tcp_route_migration.go
+++ migration/V4_add_rg_uniq_idx_tcp_route_migration.go
@@ -18,10 +18,10 @@ func (v *V4AddRgUniqIdxTCPRoute) Version() int {
 }
 
 func (v *V4AddRgUniqIdxTCPRoute) Run(sqlDB *db.SqlDB) error {
-	_, err := sqlDB.Client.Model(&models.TcpRouteMapping{}).RemoveIndex("idx_tcp_route")
+	err := sqlDB.Client.Model(&models.TcpRouteMapping{}).RemoveIndex("idx_tcp_route")
 	if err != nil {
 		return err
 	}
-	_, err = sqlDB.Client.Model(&models.TcpRouteMapping{}).AddUniqueIndex("idx_tcp_route", "router_group_guid", "host_port", "host_ip", "external_port")
+	err = sqlDB.Client.Model(&models.TcpRouteMapping{}).AddUniqueIndex("idx_tcp_route", "router_group_guid", "host_port", "host_ip", "external_port")
 	return err
 }
