73c4aa156e9fb984259cd903840ca284de084d5f
Geoff Franks <geoff.franks@broadcom.com>
2024-07-10 15:32:17 +0000

    Revert "fix linting message"
    This reverts commit 15276a7966dec16c044760a70fc859e3b3cbbf85.

--- cmd/routing-api/main.go
+++ cmd/routing-api/main.go
@@ -89,7 +89,7 @@ func main() {
 	}
 
 	prefix := "routing_api"
-	//lint:ignore SA1019 - we will fix in the future
+	// lint :ignore SA1019
 	statsdClient, err := statsd.NewBufferedClient(cfg.StatsdEndpoint, prefix, cfg.StatsdClientFlushInterval, 512)
 	if err != nil {
 		logger.Error("failed-to-create-statsd-client", err)
