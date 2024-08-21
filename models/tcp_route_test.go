90768c6c7aac7c4254dc31c1ee4254d88a0da5d9
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2024-08-21 10:05:13 -0700

    Rename pointertoString to pointerToString

--- models/tcp_route_test.go
+++ models/tcp_route_test.go
@@ -9,7 +9,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func pointertoString(s string) *string { return &s }
+func pointerToString(s string) *string { return &s }
 
 var _ = Describe("TCP Route", func() {
 	Describe("TcpMappingEntity", func() {
@@ -36,7 +36,7 @@ var _ = Describe("TCP Route", func() {
 
 			Context("when a valid SNI hostname is provided", func() {
 				BeforeEach(func() {
-					sniHostNamePtr = pointertoString("sniHostname")
+					sniHostNamePtr = pointerToString("sniHostname")
 				})
 
 				It("Accepts the value", func() {
@@ -50,7 +50,7 @@ var _ = Describe("TCP Route", func() {
 			})
 			Context("when the SNI hostname is empty", func() {
 				BeforeEach(func() {
-					sniHostNamePtr = pointertoString("")
+					sniHostNamePtr = pointerToString("")
 				})
 				It("is provided in the marshaled JSON", func() {
 					j, err := json.Marshal(tcpRouteMapping)
@@ -64,7 +64,7 @@ var _ = Describe("TCP Route", func() {
 			var sniHostNamePtr2 *string
 
 			BeforeEach(func() {
-				sniHostNamePtr = pointertoString("sniHostName")
+				sniHostNamePtr = pointerToString("sniHostName")
 			})
 
 			JustBeforeEach(func() {
@@ -81,7 +81,7 @@ var _ = Describe("TCP Route", func() {
 			})
 			Context("when two routes have equal values", func() {
 				BeforeEach(func() {
-					sniHostNamePtr2 = pointertoString("sniHostName")
+					sniHostNamePtr2 = pointerToString("sniHostName")
 				})
 				It("matches", func() {
 					Expect(tcpRouteMapping.Matches(tcpRouteMapping2)).To(BeTrue())
@@ -90,7 +90,7 @@ var _ = Describe("TCP Route", func() {
 
 			Context("when two routes have values that are not equal", func() {
 				BeforeEach(func() {
-					sniHostNamePtr2 = pointertoString("sniHostName2")
+					sniHostNamePtr2 = pointerToString("sniHostName2")
 				})
 				It("doesn't match", func() {
 					Expect(tcpRouteMapping.Matches(tcpRouteMapping2)).To(BeFalse())
